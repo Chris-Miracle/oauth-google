1620ffa2db0ddcd4e4e6ea40db4d3c55433bc313
Chris Miracle <55501479+Chris-Miracle@users.noreply.github.com>
2021-08-26 11:15:23 +0100

    initial commit

---
+++ redirect.php
@@ -0,0 +1,48 @@
+<?php
+require_once 'vendor/autoload.php';
+  
+// init configuration
+$clientID = '534290604425-elo1t7b6jo6jclf6lm85ha1mqi5j26p0.apps.googleusercontent.com';
+$clientSecret = 'rWN9PXLvxUoogPnmU9R5dItp';
+$redirectUri = 'http://localhost/redirect.php';
+   
+// create Client Request to access Google API
+$client = new Google_Client();
+$client->setClientId($clientID);
+$client->setClientSecret($clientSecret);
+$client->setRedirectUri($redirectUri);
+$client->addScope("email");
+$client->addScope("profile");
+  
+// authenticate code from Google OAuth Flow
+if (isset($_GET['code'])) {
+  $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+  $client->setAccessToken($token['access_token']);
+   
+  // get profile info
+  $google_oauth = new Google_Service_Oauth2($client);
+  $google_account_info = $google_oauth->userinfo->get();
+  $email =  $google_account_info->email;
+  $name =  $google_account_info->name;
+
+  $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
+
+    $hostname = $dbparts['host-axocheck'];
+    $username = $dbparts['user-chris'];
+    $password = $dbparts['pass-chris'];
+  
+    // Create connection
+    $conn = new mysqli($hostname, $username, $password, $database);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $user_details = array_merge($email, $name);
+
+} else {
+  echo "<a href='".$client->createAuthUrl()."'>Google Login</a>";
+}
+?>
